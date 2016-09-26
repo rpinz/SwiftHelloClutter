06f1435ed30dfdbc16313a5d9a3be165975b0f61
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-26 15:40:37 +0200

    Stage 5: rectangles rotating with the time line

--- Sources/main.swift
+++ Sources/main.swift
@@ -36,6 +36,20 @@ let purple = Color(red: 255, green: 0,   blue: 255, alpha: 128 )
 let colours = [red, green, blue, yellow, cyan, purple]
 let rectangles = colours.map { createRectOn(stage, colour: $0) }
 
+var rotation = 0.0
+let timeLine = Timeline(msecs: 60)
+
+timeLine.setRepeat(count: -1)
+timeLine.onNewFrame { _,_,_ in
+    rotation += 0.3;
+
+    let n = rectangles.count
+    for i in 0..<n {
+        rectangles[i].setRotationAngle(axis:.z_axis, angle: rotation * Double(n-i-1))
+    }
+}
+timeLine.start()
+
 stage.show()
 
 main()
