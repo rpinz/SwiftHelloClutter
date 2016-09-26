7599da638cc9551ff0980d99aeb2611983492be9
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-26 13:14:38 +0200

    Stage 2: add a green rectangle

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,6 +10,16 @@ let stage = Stage()
 
 stage.setSize(width: 512, height: 512)
 stage.setBackground(color: black)
+
+let translucent_green = Color(red: 0, green: 255, blue: 0, alpha: 128)
+let rect = Rectangle(color: translucent_green)
+
+rect.setSize(width: 100, height: 100)
+rect.setPosition(x: 100, y: 100)
+
+stage.add(child: rect)
+
+rect.show()
 stage.show()
 
 main()
